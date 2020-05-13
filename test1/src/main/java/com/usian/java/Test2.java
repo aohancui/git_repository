1d0f022644a83b8a04b8d0defe434440e7056d82
aohancui <cuipanhang@qq.com>
2020-05-13 18:18:00 +0800

    修改了Test1

--- test1/src/main/java/com/usian/java/Test2.java
+++ test1/src/main/java/com/usian/java/Test2.java
@@ -1,6 +1,14 @@
 package com.usian.java;
 
 public class Test2 {
+    //github端
+    public static void main(String[] args) {
+
+        System.out.println("hhh");
+        /*
+        修改了1.0的bug
+         */
+    }
 
 
 }
