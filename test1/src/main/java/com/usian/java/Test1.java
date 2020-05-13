714c5e54f495162d0b17e6eae52b06cbc1ca950b
崔傲寒 <65185833+aohancui@users.noreply.github.com>
2020-05-13 17:36:51 +0800

    Update Test1.java

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,7 +1,7 @@
 package com.usian.java;
 
 public class Test1{
-    //修改Test1
+    //远程仓库上传的代码
     public static void main(String[] args) {
       //github端（其他同事）修改了代码
         System.out.println("世界你好");
