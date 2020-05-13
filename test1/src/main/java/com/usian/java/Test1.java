1734b742dc3e66a1dd8022eaf8a337e409b59146
aohancui <cuipanhang@qq.com>
2020-05-13 17:38:10 +0800

    修改了Test1

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,7 +1,7 @@
 package com.usian.java;
 
 public class Test1{
-    //修改Test1
+    //本地idea工作空间的代码
     public static void main(String[] args) {
       //github端（其他同事）修改了代码
         System.out.println("世界你好");
